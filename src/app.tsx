899987378cf24751ea9908d0188988c96a0ef84c
alheym <alheym.mw@gmail.com>
2023-09-17 10:46:46 +0300

    init project

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import HelloWorld from './component/HelloWorld'
+
+const container = document.getElementById('root') as HTMLElement
+
+const initialChildren = (
+  <StrictMode>
+    <HelloWorld />
+  </StrictMode>
+)
+
+const root = createRoot(container)
+root.render(initialChildren)
